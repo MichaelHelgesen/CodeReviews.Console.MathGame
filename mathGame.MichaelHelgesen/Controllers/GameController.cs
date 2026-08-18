a8bdf7102f84eb565bda8a3d762caad8e4ebc5ed
Michael Helgesen <michaelhelgesen02@gmail.com>
2026-08-18 22:40:55 +0200

    feat(GameController): Added Start Message method

--- mathGame.MichaelHelgesen/Controllers/GameController.cs
+++ mathGame.MichaelHelgesen/Controllers/GameController.cs
@@ -1,8 +1,24 @@
+using mathGame.MichaelHelgesen.Enums;
+using Spectre.Console;
+
 namespace mathGame.MichaelHelgesen.Controllers;
 public class GameController()
 {
-    public static void StartGame()
+    // DONE: Display message
+    // Play 5 rounds
+    // Archive game
+
+    internal static void PlayGame(MenuItems gameType)
     {
-        Console.WriteLine("Spiller");
+        DisplayStartMessage(gameType);
+        PlayGame(gameType);
     }
-} 
+
+    internal static void DisplayStartMessage(MenuItems gameType)
+    {
+        AnsiConsole.Prompt(
+    new TextPrompt<string>($"You selected: [yellow]{gameType}[/]. [grey]Press [bold]Enter[/] to continue...[/]")
+        .AllowEmpty()
+);
+    }
+}
--- mathGame.MichaelHelgesen/Controllers/MenuController.cs
+++ mathGame.MichaelHelgesen/Controllers/MenuController.cs
@@ -2,14 +2,16 @@ namespace mathGame.MichaelHelgesen.Controllers;
 using mathGame.MichaelHelgesen.Enums;
 using Spectre.Console;
 
+// DONE: Users should be presented with a menu to choose an operation
 
-//You need to create a game that consists of asking the player what's the result of a math question (i.e. 9 x 9 = ?), collecting the input and adding a point in case of a correct answer.
-//A game needs to have at least 5 questions.
-//The divisions should result on INTEGERS ONLY and dividends should go from 0 to 100. Example: Your app shouldn't present the division 7/2 to the user, since it doesn't result in an integer.
-//Users should be presented with a menu to choose an operation
-//You should record previous games in a List and there should be an option in the menu for the user to visualize a history of previous games.
-//You don't need to record results on a database. Once the program is closed the results will be deleted.
-
+// TODO: You need to create a game that consists of asking the player what's the result of a math question (i.e. 9 x 9 = ?), collecting the input and adding a point in case of a correct answer.
+// TODO: A game needs to have at least 5 questions.
+// TODO: The divisions should result on INTEGERS ONLY and dividends should go from 0 to 100. Example: Your app shouldn't present the division 7/2 to the user, since it doesn't result in an integer.
+// TODO: You should record previous games in a List and there should be an option in the menu for the user to visualize a history of previous games.
+// TODO: Try to implement levels of difficulty.
+// TODO: Add a timer to track how long the user takes to finish the game.
+// TODO: Create a 'Random Game' option where the players will be presented with questions from random operations
+// TODO: To follow the DRY Principle, try using just one method for all games. Additionally, double check your project and try to find opportunities to achieve the same functionality with less code, avoiding repetition when possible.
 
 internal class MenuController
 {
@@ -46,7 +48,7 @@ internal class MenuController
                 break;
             default:
                 //AnsiConsole.MarkupLine($"Spill spill");
-                GameController.StartGame();
+                GameController.PlayGame(playerChoice);
                 break;
         }
     }
