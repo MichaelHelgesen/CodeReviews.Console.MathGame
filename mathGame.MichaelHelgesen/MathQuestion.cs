fa70a549eaeb0f738eb77730edfac5bc10c93bd8
Michael Helgesen <michaelhelgesen02@gmail.com>
2026-08-24 22:04:26 +0200

    fix: deleted old files

--- mathGame.MichaelHelgesen/MathQuestion.cs
+++
@@ -1,8 +0,0 @@
-public struct MathQuestion
-{
-    public string QuestionText { get; set; }
-    public int CorrectAnswer { get; set; }
-    public int PlayerAnswer { get; set; } 
-    
-    public bool IsCorrect => PlayerAnswer == CorrectAnswer;
-}
